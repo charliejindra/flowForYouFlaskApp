15200133a61d546526aaf1a2436506755b8e7b57
charliejindra <charlessjindra@gmail.com>
2020-02-25 16:15:45 -0500

    created skeleton of about, resource, and index pages

--- flaskApp/app/routes.py
+++ flaskApp/app/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
